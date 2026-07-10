b53b01702f705559df5915221ff69c57ff058ae5
Ayush Dwivedi <ayush@bondsmart.com>
2026-07-10 17:43:19 +0530

    Add DBeaver and Codex installation scripts

--- setup.sh
+++ setup.sh
@@ -18,6 +18,8 @@ chmod +x "$ROOT_DIR"/verify.sh
 "$ROOT_DIR/install/vscode.sh"
 "$ROOT_DIR/install/cursor.sh"
 "$ROOT_DIR/install/postman.sh"
+"$ROOT_DIR/install/dbeaver.sh"
+"$ROOT_DIR/install/codex.sh"
 
 echo
 echo "Running verification..."
