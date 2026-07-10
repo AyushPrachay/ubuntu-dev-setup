2246ab41e6c00af1411b3877efa690bb24c21a3d
Ayush Dwivedi <ayush@bondsmart.com>
2026-07-10 17:50:03 +0530

    Enhance Node.js installation script
    Updated Node installation script to include additional global npm packages and improved output messages.

--- install/node.sh
+++ install/node.sh
@@ -2,7 +2,7 @@
 
 source "$(dirname "$0")/common.sh"
 
-heading "Installing Node"
+heading "Installing Node.js (LTS)"
 
 if [ ! -d "$HOME/.nvm" ]; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
@@ -10,14 +10,26 @@ fi
 
 export NVM_DIR="$HOME/.nvm"
 
+# shellcheck disable=SC1090
 source "$NVM_DIR/nvm.sh"
 
 nvm install --lts
 nvm use --lts
-nvm alias default lts/*
+nvm alias default 'lts/*'
 
-npm install -g pnpm
+echo "Installing global npm packages..."
 
+npm install -g \
+    pnpm \
+    @openai/codex \
+    npm-check-updates \
+    yarn
+
+echo
+echo "Installed versions:"
 node -v
 npm -v
 pnpm -v
+yarn -v
+codex --version || true
+ncu --version
