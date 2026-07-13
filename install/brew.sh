f01424f564fe82a0c8220ddbda8f004000a08e3b
Ayush Dwivedi <ayush@bondsmart.com>
2026-07-13 11:24:47 +0530

    Update Homebrew installation script for Linux

--- install/brew.sh
+++ install/brew.sh
@@ -4,24 +4,30 @@ source "$(dirname "$0")/common.sh"
 
 heading "Installing Homebrew"
 
-if installed brew; then
-    echo "Homebrew already installed."
+if command -v brew >/dev/null 2>&1; then
+    echo "Homebrew is already installed."
     exit 0
 fi
 
 sudo apt update
 
 sudo apt install -y \
-curl \
-git \
-build-essential \
-procps \
-file
+    build-essential \
+    bubblewrap \
+    curl \
+    file \
+    git \
+    procps
 
 NONINTERACTIVE=1 \
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# Load Homebrew for current shell
 if [ -d "/home/linuxbrew/.linuxbrew" ]; then
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
+
+    grep -qxF 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"' ~/.bashrc || \
+        echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"' >> ~/.bashrc
 fi
+
+brew --version
