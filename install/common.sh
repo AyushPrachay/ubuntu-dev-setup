1fa0d59d5a026a37c9cfb49a69c4f93341c29f42
Ayush Dwivedi <ayush@bondsmart.com>
2026-07-13 11:25:35 +0530

    Add command_exists, load_brew, and load_nvm functions
    Added functions to check command existence and load Homebrew and NVM.

--- install/common.sh
+++ install/common.sh
@@ -9,6 +9,23 @@ heading() {
     echo "========================================"
 }
 
-installed() {
+command_exists() {
     command -v "$1" >/dev/null 2>&1
+}
+
+load_brew() {
+    if [ -d "/home/linuxbrew/.linuxbrew" ]; then
+        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
+    elif [ -d "$HOME/.linuxbrew" ]; then
+        eval "$("$HOME/.linuxbrew/bin/brew" shellenv bash)"
+    fi
+}
+
+load_nvm() {
+    export NVM_DIR="$HOME/.nvm"
+
+    if [ -s "$NVM_DIR/nvm.sh" ]; then
+        # shellcheck disable=SC1090
+        . "$NVM_DIR/nvm.sh"
+    fi
 }
